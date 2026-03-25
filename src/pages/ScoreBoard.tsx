c69519255711bfe1e1342ffd62558b7a12de358b
Anshul Maske <anshul.maske@darkoinc.com>
2026-03-24 20:26:04 -0400

    added confetti

--- src/pages/ScoreBoard.tsx
+++ src/pages/ScoreBoard.tsx
@@ -3,6 +3,53 @@
 import { Container, Grid, Image, Title } from '@mantine/core'
 import logo from '../assets/sequence_logo_solid.png'
 import '../styles/ScoreBoard.css'
+import confetti from 'canvas-confetti'
+
+const celebrate = () => {
+  var duration = 10 * 1000
+  var animationEnd = Date.now() + duration
+  var defaults = { startVelocity: 25, spread: 360, ticks: 60, zIndex: 0, scalar: 2 }
+
+  function randomInRange(min: number, max: number) {
+    return Math.random() * (max - min) + min
+  }
+
+  var interval = setInterval(function () {
+    var timeLeft = animationEnd - Date.now()
+
+    if (timeLeft <= 0) {
+      return clearInterval(interval)
+    }
+
+    var particleCount = 30 * (timeLeft / duration)
+    // since particles fall down, start a bit higher than random
+    confetti({
+      ...defaults,
+      particleCount,
+      origin: { x: randomInRange(0, 0.3), y: Math.random() - 0.2 },
+    })
+    confetti({
+      ...defaults,
+      particleCount,
+      origin: { x: randomInRange(0.2, 0.4), y: Math.random() - 0.2 },
+    })
+    confetti({
+      ...defaults,
+      particleCount,
+      origin: { x: randomInRange(0.4, 0.6), y: Math.random() - 0.2 },
+    })
+    confetti({
+      ...defaults,
+      particleCount,
+      origin: { x: randomInRange(0.6, 0.8), y: Math.random() - 0.2 },
+    })
+    confetti({
+      ...defaults,
+      particleCount,
+      origin: { x: randomInRange(0.8, 1.0), y: Math.random() - 0.2 },
+    })
+  }, 250)
+}
 
 export default function ScoreBoard() {
   return (
